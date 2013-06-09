eec80180da570b2bc1c91221145703fc9f1f9767
Gabriel Claudiu <gabriel@Gabriel-Claudius-MacBook-Pro.local>
2013-06-10 00:47:50 +0300

    Debug

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -83,13 +83,7 @@ class TransactionsController < ApplicationController
 
   def claim_coupon
 
-     @check = Transaction.find(:all,
-                               :select => " count(user_id) ",
-                               :conditions => " user_id = #{ params[:device_id] }
-                                                and coupon_id = #{ params[:coupon_id] }"
-                              )
-
-     unless @check
+     
 
       @buy_coupon = Transaction.create(  :user_id => params[:device_id],
                                          :coupon_id => params[:coupon_id],
@@ -105,7 +99,7 @@ class TransactionsController < ApplicationController
       @quantity.update( quantity: @difference )
       @quantity.save
        
-     end
+    
      
   end
 
